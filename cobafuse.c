33652e7acb18025fd8f7d9944e6cba6fb9d99ba8
dsteve98 <dan.steve2910@gmail.com>
2017-11-15 01:30:57 +0700

    coba temuan

--- cobafuse.c
+++ cobafuse.c
@@ -1,14 +1,28 @@
-#define FUSE_USE_VERSION 28
+#define FUSE_USE_VERSION 26
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#ifdef linux
+#define _XOPEN_SOURCE 700
+#endif
+
 #include <fuse.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
+#ifdef HAVE_SETXATTR
+#include <sys/xattr.h>
+#endif
 
-static const char *dirpath = "/home/steve/Documents";
+static const char *dirpath = "/home/steve/Downloads";
 
 static int xmp_getattr(const char *path, struct stat *stbuf)
 {
@@ -61,27 +75,62 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
 		    struct fuse_file_info *fi)
 {
-  char fpath[1000];
-	if(strcmp(path,"/") == 0)
-	{
-		path=dirpath;
-		sprintf(fpath,"%s",path);
-	}
-	else sprintf(fpath, "%s%s",dirpath,path);
-	int res = 0;
-  int fd = 0 ;
+   int fd;
+   int res;
+   char fpath[1000];
+   sprintf(fpath,"%s%s",dirpath,path);
+  
+   (void) fi;
+  
+   char ext[1000];
+   int i;
+   for(i = 0; i < strlen(fpath) && fpath[i] != '.'; i++); //mengekstrak ekstensi file
+   strcpy(ext, fpath+i);
+  
+   char cmd[1000];
+  
+   fd = open(fpath, O_RDONLY);
+   if (fd == -1)
+                  return -errno;
+   else
+   {
+      if(strcmp(ext, ".c") == 0 || strcmp(ext, ".3gp") == 0 || strcmp(ext, ".docx") == 0|| strcmp(ext, ".ditandai") == 0)
+      {
+         system("zenity --width 400 --error --title 'Error' --text 'Terjadi Kesalahan! File berisi konten berbahaya.'");
 
-	(void) fi;
-	fd = open(fpath, O_RDONLY);
-	if (fd == -1)
-		return -errno;
+         char from[1000], to[1000], newFolder[1000], filename[1000];
+        
+         //mengambil direktori dari file
+         strcpy(newFolder, fpath);
+        
+         //menghapus filename dan menambahkan 'rahasia'
+         for(i = strlen(newFolder) - 1; newFolder[i] != '/'; newFolder[i--] = 0);
+         strcat(newFolder, "rahasia");
+        
+         //mengambil filename
+         for(i = strlen(fpath) - 1; fpath[i] != '/'; i--);
+         strcpy(filename, fpath+(i+1) ); //+1 untuk menghilangkan tanda ‘/’
+        
+         //Mengecek ada tidaknya folder rahasia
+         struct stat s;
+         if (stat(newFolder, &s) != 0)  //0 berarti folder tersebut tidak ada
+                        mkdir(newFolder, 0777); //membuat folder 'rahasia' folder ke direktori mountnya
+        
+         sprintf(from, "%s", fpath);  //mengambil path file awal
+         sprintf(to, "%s/%s.ditandai", newFolder, filename); //membuat path tujuan pemindahan file ke folder ‘rahasia’ dan menambahkan ekstensi ‘.ditandai’
+        
+         sprintf(cmd, "mv %s %s", from, to); //Merename dan memindah file
+         system(cmd);
+         return -errno;                                   
+        }
 
-	res = pread(fd, buf, size, offset);
-	if (res == -1)
-		res = -errno;
+      res = pread(fd, buf, size, offset);
+      if (res == -1)
+                     res = -errno;
 
-	close(fd);
-	return res;
+      close(fd);
+   }
+   return res;
 }
 
 static struct fuse_operations xmp_oper = {
